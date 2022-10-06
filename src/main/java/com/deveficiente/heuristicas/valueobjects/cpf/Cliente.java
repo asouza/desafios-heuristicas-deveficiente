6b952d804184731c82424ef39045525869692d03
Alberto <alberto.tavares@zup.com.br>
2022-10-06 18:26:21 -0300

    feat(value-objects): desafio para a pessoa refatorar a passagem docpf

---
+++ src/main/java/com/deveficiente/heuristicas/valueobjects/cpf/Cliente.java
@@ -0,0 +1,13 @@
+package com.deveficiente.heuristicas.valueobjects.cpf;
+
+public class Cliente {
+
+	private String cpf;
+
+	public Cliente(String cpf) {
+		super();
+		this.cpf = cpf;
+	}
+	
+	
+}
