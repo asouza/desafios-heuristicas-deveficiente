7201cc3d7139f2c53812141d190176719ef8667b
Alberto <alberto.tavares@zup.com.br>
2022-10-06 17:40:31 -0300

    comeco do projeto

---
+++ src/main/java/com/deveficiente/heuristicas/DeveficienteExemplosHeuristicasOoApplication.java
@@ -0,0 +1,13 @@
+package com.deveficiente.heuristicas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeveficienteExemplosHeuristicasOoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeveficienteExemplosHeuristicasOoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deveficiente/threadsvirtuais/DeveficienteExemplosHeuristicasOoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deveficiente.threadsvirtuais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeveficienteExemplosHeuristicasOoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
