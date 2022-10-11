ca8c1edc2d18506f588ee188a42598b6f7551c76
Alberto <alberto.tavares@zup.com.br>
2022-10-11 18:33:38 -0300

    feat(enumsricas) equals para a atividade

--- src/main/java/com/deveficiente/heuristicas/enumsricas/statusv1/Resposta.java
+++ src/main/java/com/deveficiente/heuristicas/enumsricas/statusv1/Resposta.java
@@ -9,4 +9,32 @@ public class Resposta {
 		this.atividade = atividade;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((atividade == null) ? 0 : atividade.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Resposta other = (Resposta) obj;
+		if (atividade == null) {
+			if (other.atividade != null)
+				return false;
+		} else if (!atividade.equals(other.atividade))
+			return false;
+		return true;
+	}
+	
+	
+
 }
