3fda3478d9e712373c34f36635fc0eeb7d099be0
Alberto <alberto.tavares@zup.com.br>
2022-10-13 17:26:13 -0300

    feat(invertederdependencia) cenario para criar nova secao, faltou salvar o controller do markdown

--- src/main/java/com/deveficiente/heuristicas/inverterdependencia/criacaosecoesatividades/v1/ImportaSecaoViaMarkdownController.java
+++ src/main/java/com/deveficiente/heuristicas/inverterdependencia/criacaosecoesatividades/v1/ImportaSecaoViaMarkdownController.java
@@ -19,6 +19,8 @@ public class ImportaSecaoViaMarkdownController {
 		novaSecaoMarkdown.adicionaTeoria("titulo1",1);
 		novaSecaoMarkdown.adicionaAtividade("titulo 1",1,TipoAtividade.OBRIGATORIA);
 		
+		novaSecaoMarkdown.toSecao();
+		
 		
 	}
 }
